8057c48d876917775d2768b01cb882974ff0e65b
iamqazi <qaziobaid7897@gmail.com>
2025-04-28 20:28:18 +0500

    changes

--- slider.js
+++ slider.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   new Swiper(".section-slider__carousel", {
     loop: true,
-    speed: 600,
+    speed: 300,
     spaceBetween: 20,
     autoplay: {
       delay: 3000,
@@ -13,5 +13,17 @@ document.addEventListener("DOMContentLoaded", function () {
       bulletClass: "section-slider__dot",
       bulletActiveClass: "active",
     },
+  });
+});
+const slider = document.querySelector(".mtc-slider");
+const dots = document.querySelectorAll(".mtc-dot");
+
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    const translateValue = index * -100;
+    slider.style.transform = `translateX(${translateValue}%)`;
+
+    dots.forEach((d) => d.classList.remove("active"));
+    dot.classList.add("active");
   });
 });
